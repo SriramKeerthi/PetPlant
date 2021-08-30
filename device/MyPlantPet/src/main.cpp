52e9abfa0b9a3364d977ae88b0d54c46acb68e7d
Sriram Kunjathur <shramk@gmail.com>
2021-08-30 18:46:48 +0200

    Add code to detect touch and update screen

--- device/MyPlantPet/src/main.cpp
+++ device/MyPlantPet/src/main.cpp
@@ -5,10 +5,17 @@
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
 #include <Preferences.h>
+#include <Button2.h>
 
 /* Screen Settings */
-#define LED_CHANNEL 1
-#define LED_PIN     4
+#define LED_CHANNEL    1
+#define LED_PIN        4
+
+/* Input Settings */
+#define TOUCH_1        12
+#define TOUCH_2        13
+#define BUTTON_1       0
+#define BUTTON_2       35
 
 /* BLE Settings */
 #define BLE_SCAN_TIME  30
@@ -24,6 +31,8 @@
 TFT_eSPI tft = TFT_eSPI(135, 240);
 Preferences pref;
 BLEScan* pBLEScan;
+Button2 button1 = Button2(BUTTON_1);
+Button2 button2 = Button2(BUTTON_2);
 
 void clearScreen(int red = 0x4B, int green = 0xAB, int blue = 0x5C) {
     tft.fillScreen(_RGB(red, green, blue));
@@ -105,6 +114,13 @@ void scanForDevices( void * pvParameters ) {
     }
 }
 
+int t1Base = 10;
+int t2Base = 10;
+void buttonClickHandler(Button2& btn) {
+    t1Base = touchRead(TOUCH_1);
+    t2Base = touchRead(TOUCH_2);
+}
+
 void setup() {
     Serial.begin(115200);
     delay(100);
@@ -120,7 +136,17 @@ void setup() {
         pref.putInt(CFG_CODE, random(10000)); // Generate a random number between 0 - 10000
     }
 
-    // Set up screen
+    Serial.println("Setting up buttons...");
+    button1.setClickHandler(buttonClickHandler);
+    button1.setLongClickHandler(buttonClickHandler);
+    button1.setDoubleClickHandler(buttonClickHandler);
+    button1.setTripleClickHandler(buttonClickHandler);
+    button2.setClickHandler(buttonClickHandler);
+    button2.setLongClickHandler(buttonClickHandler);
+    button2.setDoubleClickHandler(buttonClickHandler);
+    button2.setTripleClickHandler(buttonClickHandler);
+
+    Serial.println("Setting up screen...");
     ledcSetup(LED_CHANNEL, 5000, 8);
     ledcAttachPin(LED_PIN, LED_CHANNEL);
     tft.init();
@@ -148,15 +174,75 @@ void setup() {
         1);
 }
 
+char touchVal(int base, int val) {
+    if (val < base - 5) {
+        return 2;
+    }
+    if (val < base - 2) {
+        return 1;
+    }
+    return ' ' - 175;
+}
+
+char isTouched(int base, int val) {
+    return val < base - 5;
+}
+
+int r = 0x4B;
+int g = 0xAB;
+int b = 0x5C;
+long refreshRate = 100;
+long printRate = 500;
+long logRate = 200;
+char* msg = new char[100];
+char hl[] = {253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 0};
+long lastPrint = millis();
+long lastLog = millis();
 void loop() {
+    long startTime = millis();
+    button1.loop();
+    button2.loop();
+
+    int t1 = touchRead(TOUCH_1);
+    if (t1 > t1Base)
+        t1Base = t1;
+
+    int t2 = touchRead(TOUCH_2);
+    if (t2 > t2Base)
+        t2Base = t2;
+
+    // Something went wrong with the read, retry method
+    if (t1 == 0 || t2 == 0)
+        return;
+
     if (!pref.isKey(CFG_OWNER_UUID)) {
         ledcWrite(LED_CHANNEL, 255);
+        tft.setTextSize(7);
         tft.setCursor(42,42);
         tft.printf("%04d", pref.getInt(CFG_CODE));
     } else {
         if (millis() - lastDeviceNear > 5000) {
             ledcWrite(LED_CHANNEL, 0);
+        } else {
+            sprintf(msg, " MyPlantPet\n%s\n\n T1: %02d/%02d %c\n T2: %02d/%02d %c\n", hl, t1, t1Base, 175 + touchVal(t1Base, t1), t2, t2Base, 175 + touchVal(t2Base, t2));
+            if (isTouched(t1Base, t1) && isTouched(t2Base, t2)) {
+                r = random(0,255);
+                g = random(0,255);
+                b = random(0,255);
+                clearScreen(r,g,b);
+                lastPrint = 0;
+            }
+            if (millis() - lastLog > logRate) {
+                Serial.println(msg);
+                lastLog = millis();
+            }
+            if (millis() - lastPrint > printRate) {
+                lastPrint = millis();
+                tft.setTextSize(3);
+                tft.setCursor(0,0);
+                tft.printf(msg);
+            }
         }
     }
-    delay(100);
+    delay(max(0ul, refreshRate - (millis() - startTime)));
 }
