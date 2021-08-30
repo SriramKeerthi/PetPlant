a13a868a9f64e95a2a20c481c708291f1fe2776e
Sriram Kunjathur <shramk@gmail.com>
2021-08-30 14:39:39 +0200

    Initial commit with empty device code

---
+++ device/PetPlant/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    delay(100);
+    Serial.println("\n\n\n************************\nPETPLANT DEVICE STARTED\n************************\nInitializing...");
+}
+
+void loop() {
+}
