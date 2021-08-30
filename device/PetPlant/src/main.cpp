662aebab7e3eb9c1ba5390d73d88e6478b1bdab6
Sriram Kunjathur <shramk@gmail.com>
2021-08-30 15:03:06 +0200

    Move BLE Scanning to different core, add preferences check for owner

--- device/PetPlant/src/main.cpp
+++ device/PetPlant/src/main.cpp
@@ -4,11 +4,17 @@
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+#include <Preferences.h>
 
 /* BLE Settings */
 #define BLE_SCAN_TIME  30
 #define BLE_INTERVAL   500
 #define BLE_WINDOW     499
+
+/* Config Strings */
+#define CFG_OWNER_UUID "OWNER_UUID"
+
+Preferences pref;
 
 BLEScan* pBLEScan;
 
@@ -20,7 +26,7 @@ class PetPlantCallbacks: public BLEAdvertisedDeviceCallbacks {
     }
 };
 
-void scanForDevices() {
+void scanForDevices( void * pvParameters ) {
     while(true) {
         Serial.println("Scanning...");
         pBLEScan->start(BLE_SCAN_TIME, false);
@@ -33,16 +39,34 @@ void setup() {
     delay(100);
     Serial.println("\n\n\n***********************\nPETPLANT DEVICE STARTED\n***********************\nInitializing...");
 
+    Serial.println("Checking for owner...");
+    pref.begin("petplant");
+    if (pref.isKey(CFG_OWNER_UUID)) {
+        Serial.printf("Owner exists: %s\n", pref.getString(CFG_OWNER_UUID).c_str());
+    } else {
+        Serial.println("No owner found");
+    }
+    
     Serial.println("Staring BLE Scanning...");
     // Initialize BLE device
     BLEDevice::init("Pet Plant");
     pBLEScan = BLEDevice::getScan();
     pBLEScan->setAdvertisedDeviceCallbacks(new PetPlantCallbacks());
     pBLEScan->setActiveScan(true);
-    pBLEScan->setInterval(500);
-    pBLEScan->setWindow(499);
+    pBLEScan->setInterval(BLE_INTERVAL);
+    pBLEScan->setWindow(BLE_WINDOW);
+
+    // Scan for devices in a different core
+    xTaskCreatePinnedToCore(
+        scanForDevices,
+        "scanForDevices",
+        10000,
+        NULL,
+        0,
+        NULL,
+        1);
 }
 
 void loop() {
-    scanForDevices();
+    delay(100);
 }
