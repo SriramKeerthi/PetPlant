9a95d6b9500c9898043439c483f46bf8160d5695
Sriram Kunjathur <shramk@gmail.com>
2021-09-01 09:49:21 +0200

    Clean up UI

--- app/MyPlantPet/App.tsx
+++ app/MyPlantPet/App.tsx
@@ -1,23 +1,24 @@
-import { light as lightTheme, mapping } from '@eva-design/eva';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-const Stack = createNativeStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#219446aa',
+    accent: '#7ac29766',
+    text: "#fff"
+  },
+};
 export default function App() {
-  return <>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider mapping={mapping}
-      theme={lightTheme}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='My Plant Pet!' component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApplicationProvider>
-  </>;
+  return <PaperProvider theme={theme}>
+    < HomeScreen />
+  </PaperProvider>
+
+
 }
--- app/MyPlantPet/src/screens/HomeScreen.tsx
+++ app/MyPlantPet/src/screens/HomeScreen.tsx
@@ -1,17 +1,19 @@
 import BeaconBroadcast from '@jaidis/react-native-ibeacon-simulator';
-import { Button, Input, Text } from '@ui-kitten/components';
 import React, { useEffect, useState } from 'react';
-import { Alert, Platform, View } from 'react-native';
+import { Alert, Platform, View, ImageBackground, StyleSheet, } from 'react-native';
 import * as AuthSession from 'expo-auth-session';
 import jwtDecode from 'jwt-decode';
 import credentials from '../config/auth0-configuration';
 import getUuid from 'uuid-by-string';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { Asset } from "expo-asset";
+import { Button, Text, } from 'react-native-paper';
 const authorizationEndpoint = "https://myplantpet.eu.auth0.com/authorize";
-
+import { StatusBar } from 'expo-status-bar';
+import SmoothPinCodeInput from "react-native-smooth-pincode-input";
 const useProxy = Platform.select({ web: false, default: true });
 const redirectUri = AuthSession.makeRedirectUri({ useProxy });
+import { BlurView } from 'expo-blur';
 
 type JWTToken = {
     aud: string,
@@ -129,35 +131,124 @@ const HomeScreen = () => {
         setMinor(undefined);
     }
 
+    console.log({ code })
     return (
-        <View style={{ padding: 20 }}>
-            {!!uuid && <>{beaconState == 0 &&
+
+        <View style={styles.container}>
+
+            <ImageBackground source={{
+                uri: Asset.fromModule(require("../../assets/1.jpg")).uri,
+
+            }}
+                blurRadius={1.5}
+
+                style={styles.image}>
+                <View style={styles.header}><Text style={styles.headerText}>My Plant Pet</Text></View>
+
+                <View style={styles.content}>{!!uuid && <>{
+                    beaconState == 0 &&
                 <>
-                    <Input size='large' placeholder="Enter your plant's code" onSubmitEditing={connect} onChangeText={setCode} value={code} />
-                    <Button disabled={code.length != 4} onPress={connect}>Start Petting!</Button>
+                        <SmoothPinCodeInput
+                            value={code}
+                            onTextChange={setCode}
+                            codeLength={4}
+
+                            restrictToNumbers
+                            containerStyle={styles.pinInput}
+                            cellStyle={styles.cell}
+                            textStyle={styles.cellText}
+                            cellStyleFocused={styles.cellFocused}
+                            textStyleFocused={styles.cellFocusedText}
+                        // onBackspace={this._focusePrevInput}
+                        />
+
+
+                        {code.length === 4 && <Button onPress={connect} mode="contained">Start Petting!</Button>}
+                        <Button onPress={logout}>Logout</Button>
                 </>
             }
                 {beaconState == 1 &&
                     <>
-                        <Text style={{ alignSelf: "center" }}>Ready to pet your plant!</Text>
-                        <Button onPress={disconnect}>Disconnect</Button>
+                        <Text style={styles.contentText}>You are good to go! Pet away!</Text>
+                        <View style={styles.horizontal}><Button onPress={disconnect}>Disconnect</Button><Button onPress={logout}>Logout</Button></View>
                     </>
                 }
                 {
                     beaconState == 2 &&
                     <>
-                        <Text style={{ alignSelf: "center" }}>Bring your phone near the plant and click done when the plant acknowledges!</Text>
-                        <Button onPress={reset}>Done!</Button>
+                            <Text style={styles.contentText}>Bring your phone near the plant and click done when the plant acknowledges!</Text>
+                            <View style={styles.horizontal}><Button onPress={reset}>Done!</Button><Button onPress={logout}>Logout</Button></View>
                     </>
-                }
-                <Button style={{ marginTop: 20 }} onPress={logout}>Logout</Button></>}
-            {!uuid &&
-                <>
-                <Text style={{ alignSelf: "center" }}>You're not logged in</Text>
-                    <Button onPress={() => promptAsync({ useProxy })}>Login</Button>
-                </>}
+                    }</>}
+                    {!uuid &&
+                        <>
+                            <Text style={styles.contentText}>Login to interact with your plant!</Text>
+                            <Button mode='contained' onPress={() => promptAsync({ useProxy })} style={styles.mainButton} labelStyle={styles.mainButtonContent}>Login</Button>
+                        </>}
+                </View>
+            </ImageBackground>
+            <StatusBar style="light" />
         </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: "space-between", },
+    header: { alignContent: "center", alignSelf: "center", marginBottom: "auto", marginTop: 100, },
+    headerText: { fontSize: 36 },
+    image: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+
+    },
+    content: {
+        alignContent: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#000000cc",
+        padding: 70,
+    },
+    contentText: { fontSize: 24, marginTop: 40, marginBottom: 40, textAlign: "center", color: "white" },
+    pinInput: {
+        alignSelf: "center",
+        marginBottom: 100,
+    },
+    cell: {
+        height: 100,
+        borderColor: "white",
+        borderBottomWidth: 2,
+        color: "white"
+    },
+    cellText: {
+        color: "white"
+    },
+
+    cellFocused: {
+        borderBottomWidth: 1,
+        height: 100,
+        borderColor: "green"
+    },
+    cellFocusedText: { color: "red" },
+
+    mainButton: {
+        marginTop: 10,
+        marginBottom: 10,
+        width: "100%"
+    },
+
+    mainButtonContent: {
+        color: 'white',
+        fontSize: 18
+    },
+    horizontal: {
+        flexDirection: 'row'
+        ,
+        alignItems: "center"
+    }
+
+
+});
 
 export default HomeScreen
